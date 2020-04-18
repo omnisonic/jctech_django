6142fdefa1000fdc33c1ddc7a6dc03295568ea04
John <omnisonic@gmail.com>
2020-04-17 17:37:01 -0700

    used api, json data, template filters to add list of github repos to procects page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, ContactPageView, BlogPageView  #new
+from pages import views
 
 
 urlpatterns = [
-    path ('', HomePageView.as_view(), name='home')
+    path ('about', AboutPageView.as_view(), name='about'),
+    path ('blog', BlogPageView.as_view(), name='blog'),
+    path ('projects', views.projects, name='projects'),
+    path ('contact', ContactPageView.as_view(), name='contact'),
+    path ('', HomePageView.as_view(), name='home'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,48 @@
+import requests
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 # Create your views here.
-
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-def homepage(request):
-    context = {}
-    return render(request, 'home.html', context)
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class BlogPageView(TemplateView):
+    template_name = 'blog.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+# class ProjectsPageView(TemplateView):
+#     template_name = 'projects.html'
+#     # https://api.github.com/users/omnisonic/repos
+
+
+def projects(request):
+    response = requests.get('https://api.github.com/users/omnisonic/repos')
+    repos = response.json()
+    repo_list = repos[0]['html_url']
+    results = []
+    for item in repos:
+        results.append(item['html_url'])
+    print('hello\n' + '\n'.join(results))
+    str1 = '\n '.join(results)
+
+        
+    context = {
+        'github_repos': str1,
+    }
+    return render(request, 'projects.html', context)
+
+
+def getGithubLinks(request):
+    response = requests.get('https://api.github.com/users/omnisonic/repos')
+    repos = response.json()
+    repo_list = repos[0]['html_url']
+    for i in repos:
+        repo_list += [i]
+        return repo_list
+
